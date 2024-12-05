f539e32abe18dbed5001ce89a0786b728e9b5aec
arseniy_bokarev <arsenyam652@gmail.com>
2024-12-05 16:49:33 +0300

    add def index_page, index.html

---
+++ watchmanofdoom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for watchmanofdoom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchmanofdoom.settings')
+
+application = get_wsgi_application()
